67d8fb9a13e81b87bbe5754b2759cdfc97bdaaaf
Saksham Rathi <22b1003@iitb.ac.in>
2025-07-14 20:13:16 +0530

    tac cc started

--- common-headers.hh
+++ common-headers.hh
@@ -23,7 +23,7 @@ using namespace std;
 #include "asm.hh"
 
 void add_variables_to_global_symtab(vec_var_type*);
-void add_functions_to_loca_symtab(vec_func_type*);
+void add_variables_to_local_symtab(vec_func_type*);
 void throw_error_and_exit(string error_msg);
 void print_ast();
 void print_tac();
